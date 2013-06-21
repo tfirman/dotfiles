86d5c6258f5b65a879a4b28a430fd803d78f3a60
Carl Taylor <ctaylor@fulcrum.net>
2013-06-21 10:36:15 -0700

    Fixed if statements, ln arg order

--- install.sh
+++ install.sh
@@ -3,28 +3,28 @@ TS=$(date +%s)
 
 #####################################
 #
-if [ -e $HOME/.vimrc ] then
+if [ -e $HOME/.vimrc ]; then
   mv $HOME/.vimrc $HOME/.vimrc-old-$TS
 fi
-ln -s $HOME/.vimrc $HOME/.dotfiles/vimrc
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc 
 
-if [ -e $HOME/.vim ] then
+if [ -e $HOME/.vim ]; then
   mv $HOME/.vim $HOME/.vim-old-$TS
 fi
-ln -s $HOME/.vim $HOME/.dotfiles/vim
+ln -s $HOME/.dotfiles/vim $HOME/.vim 
 
 #####################################
 #
 mkdir -p $HOME/.config
-if [ -e $HOME/.config/fish ] then
+if [ -e $HOME/.config/fish ]; then
   mv $HOME/.config/fish $HOME/.config/fish-old-$TS
 fi
-ln -s $HOME/.config/fish $HOME/.dotfiles/fish
+ln -s $HOME/.dotfiles/fish $HOME/.config/fish 
 
 #####################################
 #
-if [ -e $HOME/.tmux.conf ] then
+if [ -e $HOME/.tmux.conf ]; then
   mv $HOME/.tmux.conf $HOME/.tmux.conf-old-$TS
 fi
-ln -s $HOME/.tmux.conf $HOME/.dotfiles/tmux.conf
+ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf 
 
