321f6dc5ac6e4fa707521fb1ca95118ed2b047b8
Carl Taylor <ctaylor@fulcrum.net>
2013-11-05 10:53:05 -0800

    Added path for haskell cabal

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+# Globals
+set -x EDITOR 'vim'
+
+# Dotfiles bins
+set PATH (dirname (status -f))"/../bin" $PATH
+
 # Ruby optimizations
 set -x RUBY_HEAP_MIN_SLOTS 800000
 set -x RUBY_HEAP_FREE_MIN 100000
@@ -5,20 +11,22 @@ set -x RUBY_HEAP_SLOTS_INCREMENT 300000
 set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
 set -x RUBY_GC_MALLOC_LIMIT 79000000
 
-set -x EDITOR 'vim'
-
+# Rbenv
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
+# Homebrew
 set PATH /usr/local/sbin $PATH
 set PATH /usr/local/bin $PATH
 
-set PATH (dirname (status -f))"/../bin" $PATH
-
+# Aliases
 alias z "zeus"
 alias b "bundle exec"
 
+# Scala sbt opts
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
+# Haskell bins
+set PATH $HOME/.cabal/bin $PATH
 
