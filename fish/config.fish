089fb5b07649936c20644342b1dacb9918617ef7
Carl Taylor <ctaylor@fulcrum.net>
2013-11-05 10:50:23 -0800

    Increased history, added bin to path in git/config instead of linking executibles

--- fish/config.fish
+++ fish/config.fish
@@ -5,11 +5,20 @@ set -x RUBY_HEAP_SLOTS_INCREMENT 300000
 set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
 set -x RUBY_GC_MALLOC_LIMIT 79000000
 
+set -x EDITOR 'vim'
+
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/bin $PATH
+
+set PATH (dirname (status -f))"/../bin" $PATH
+
+alias z "zeus"
+alias b "bundle exec"
+
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
-cd .
 
