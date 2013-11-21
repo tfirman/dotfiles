a37df27ae0cd6c4334f2f62a80b334f0f1b53b5c
Carl Taylor <ctaylor@fulcrum.net>
2013-11-21 15:29:29 -0800

    Cleaned up config.fish file

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,35 @@
-# Globals
-if test -x vim
+################################################################################
+# PATHS
+
+# Dotfiles bin
+set PATH (dirname (status -f))"/../bin" $PATH
+
+# Homebrew bin
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/bin $PATH
+
+# Haskell bin
+if test -d $HOME/.cabal/bin
+  set PATH $HOME/.cabal/bin $PATH
+end
+
+# Rbenv bin
+if test -d $HOME/.rbenv
+  set PATH $HOME/.rbenv/bin $PATH
+  set PATH $HOME/.rbenv/shims $PATH
+  set PATH ./bin $PATH
+end
+
+################################################################################
+# Environment variables
+
+# Use vim as the default editor
+if type vim 1>/dev/null
   set -x EDITOR 'vim'
 end
 
-# Dotfiles bins
-set PATH (dirname (status -f))"/../bin" $PATH
-
 # Ruby optimizations
-if test -x ruby
+if type ruby > /dev/null
   set -x RUBY_HEAP_MIN_SLOTS 800000
   set -x RUBY_HEAP_FREE_MIN 100000
   set -x RUBY_HEAP_SLOTS_INCREMENT 300000
@@ -15,26 +37,12 @@ if test -x ruby
   set -x RUBY_GC_MALLOC_LIMIT 79000000
 end
 
-# Rbenv
-if test -x rbenv
-  set PATH $HOME/.rbenv/bin $PATH
-  set PATH $HOME/.rbenv/shims $PATH
-  rbenv rehash >/dev/null ^&1
-end
-
-# Homebrew
-set PATH /usr/local/sbin $PATH
-set PATH /usr/local/bin $PATH
-
-# Aliases
-alias z "zeus"
-alias b "bundle exec"
-
 # Scala sbt opts
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
-# Haskell bins
-if test -d $HOME/.cabal/bin
-  set PATH $HOME/.cabal/bin $PATH
-end
+################################################################################
+# Aliases
+
+alias z "zeus"
+alias b "bundle exec"
 
