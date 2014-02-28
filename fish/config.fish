9a24ffc479e5dc3fe8781c67487560cafe7f6740
Carl Taylor <carl@etaylor.me>
2014-02-27 21:55:57 -0800

    Fixed issue with tmux, removed resource heavy ruby optimizations

--- fish/config.fish
+++ fish/config.fish
@@ -42,15 +42,6 @@ if type vim 1>/dev/null
   set -x EDITOR 'vim'
 end
 
-# Ruby optimizations
-if type ruby > /dev/null
-  set -x RUBY_HEAP_MIN_SLOTS 800000
-  set -x RUBY_HEAP_FREE_MIN 100000
-  set -x RUBY_HEAP_SLOTS_INCREMENT 300000
-  set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
-  set -x RUBY_GC_MALLOC_LIMIT 79000000
-end
-
 # Scala sbt opts
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
