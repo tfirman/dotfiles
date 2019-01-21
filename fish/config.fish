66c67201e286dc668c88b81184a790f447dad659
Carl Taylor <ctaylor@fulcrum.net>
2019-01-21 15:32:55 -0800

    Use addPath for simplifying adding paths with parameters and not duplicating path entries

--- fish/config.fish
+++ fish/config.fish
@@ -1,92 +1,55 @@
+################################################################################
+# Functions
+
 # CD Helper
 function cd --description "Change working directory"
   builtin cd $argv
   emit cwd
 end
 
-# Remove Duplicates From Path
-function varclear --description 'Remove duplicates from environment variable'
-    if test (count $argv) = 1
-        set -l newvar
-        set -l count 0
-        for v in $$argv
-            if contains -- $v $newvar
-                set count (math $count+1)
-            else
-                set newvar $newvar $v
-            end
-        end
-        set $argv $newvar
-        test $count -gt 0
-        and echo Removed $count duplicates from $argv
-    else
-        for a in $argv
-            varclear $a
-        end
-    end
-end
+# Add path if it's not there
+function addPath --description 'Add path if it exists and is not already in PATH, also sets an ENV variable with a second param'
+  set -l dir $argv[1]
+  set -l key $argv[2]
 
+  if test -d $dir
+    if not test -z "$key"
+      set $key $dir
+    end
+    if not contains $dir $PATH
+      set PATH $PATH $dir
+    end
+  end
+end
 
 ################################################################################
-# Environment variables
+# Environment variables / Path
 
 # PWD bin
-set PATH ./bin $PATH
-set PATH ./node_modules/.bin/ $PATH
-
-# Fulcrum tools
-if test -d "$HOME/local/src/fulcrum/dev-tools/bin"
-  set PATH $HOME/local/src/fulcrum/dev-tools/bin $PATH
-end
-if test -d "$HOME/local/src/fulcrum/vm/bin"
-  set PATH $HOME/local/src/fulcrum/vm/bin $PATH
-  set -x VAGRANT_CWD $HOME/local/src/fulcrum/vm
-end
+addPath ./bin
+addPath ./node_modules/.bin
 
 # Dotfiles bin
-set PATH (dirname (status -f))"/../bin" $PATH
-set PATH $HOME/local/bin $PATH
+addPath (dirname (status -f))"/../bin"
+addPath $HOME/local/bin
 
 # Homebrew bin
-if test -d /usr/local/sbin
-  set PATH /usr/local/sbin $PATH
-end
-
-if test -d /usr/local/bin
-  set PATH /usr/local/bin $PATH
-end
+addPath /usr/local/sbin
+addPath /usr/local/bin
 
 # Android
-set -x ANDROID_HOME ~/Library/Android/sdk
-if test -d "$ANDROID_HOME/tools/bin"
-  set PATH "$ANDROID_HOME/tools/bin" $PATH
-  set PATH "$ANDROID_HOME/platform-tools" $PATH
+
+if test -d "$HOME/Library/Android/sdk"
+  set -x ANDROID_HOME ~/Library/Android/sdk
+  addPath "$ANDROID_HOME/tools/bin"
+  addPath "$ANDROID_HOME/platform-tools"
 end
 
-set -x ANDROID_NDK_HOME /usr/local/share/android-ndk
-if test -d "$ANDROID_NDK_HOME"
-  set PATH "$ANDROID_NDK_HOME" $PATH
-end
+addPath /usr/local/share/android-ndk ANDROID_NDK_HOME
 
+################################################################################
+# Git SSH
 set -x GIT_SSH git-ssh
-
-# Scala sbt opts
-set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-
-# GOPATH
-#set -x GOPATH ~/local/src/twick00/go
-
-# Gradle
-#set -x GRADLE_HOME /usr/local/bin
-
-# PATHS
-#set PATH $HOME/local/src/fulcrum/dev-tools/bin $PATH
-#set PATH $HOME/.cargo/bin $PATH
-#set PATH $HOME/Library/Android/sdk/platform-tools $PATH
-#set PATH $HOME/go/bin $PATH
-
-# Clear extra PATH variables
-varclear PATH
 
 ################################################################################
 # Make flags
@@ -95,7 +58,7 @@ set CFLAGS '-I$HOME/local/include'
 set LDFLAGS '-L$HOME/local/lib'
 
 ################################################################################
-# Misc.
+# Vim
 
 # Use vim as the default editor
 if type vim 1>/dev/null
@@ -103,7 +66,7 @@ if type vim 1>/dev/null
 end
 
 ################################################################################
-# abbreviations
+# Abbreviations
 
 # Git
 abbr g "git"
@@ -126,18 +89,21 @@ abbr gcp 'git cherry-pick'
 
 # ls
 abbr l ls
+abbr ll ls -la
 
 # Android
 abbr droid 'adb shell input text'
 
-# Maven
-abbr mvn-resolve 'mvn -q org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression='
-
 # Misc. Productivity
 abbr work 'cd ~/local/src/'
+
+# Network
 abbr port 'lsof -i tcp:'
 abbr ip 'ifconfig | grep "inet " | grep -v 127.0.0.1'
 
-if test -f ~/.config/fish/custom.fish
-  source ~/.config/fish/custom.fish
+################################################################################
+# Customization support
+
+if test -f "$HOME/.config/fish/custom.fish"
+  source "$HOME/.config/fish/custom.fish"
 end
